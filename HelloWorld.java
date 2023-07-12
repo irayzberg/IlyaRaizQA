4539a166bdc6a01a249f5dc17fc56f83334cea34
irayzberg <irayzberg0704@gmail.com>
2023-07-12 22:19:00 +0300

    Код Java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("It's all about quality of life");
+        System.out.println("    and finding a happy balance between work");
+        System.out.println("                    and friends and family.");
+    }
+ }
