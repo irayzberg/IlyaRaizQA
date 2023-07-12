1d74bd3ba6a742b620d57f1436b80f5fffe37d2d
irayzberg <irayzberg0704@gmail.com>
2023-07-12 22:21:21 +0300

    Изменил содержание

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("It's all about quality of life");
-        System.out.println("    and finding a happy balance between work");
-        System.out.println("                    and friends and family.");
+        System.out.println("Hello World!");
     }
  }
