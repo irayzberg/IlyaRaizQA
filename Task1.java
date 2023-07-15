200ae08577d91efecf27a6da95b73178a0dad008
irayzberg <irayzberg0704@gmail.com>
2023-07-15 15:49:32 +0300

    Variable types

---
+++ Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args) {
+        byte age1 = 5;
+        byte age2 = 10;
+        System.out.println(age1+age2);
+        short kaput = 32000;
+        short product = -32000; 
+        System.out.println(kaput + product);
+
+  }
+}
